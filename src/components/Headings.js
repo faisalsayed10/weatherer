c1a492e62f25b0525e5e5fd2e9321e4dca561223
faisalsayed10 <faisal.sayed502@gmail.com>
2020-10-03 13:13:35 +0530

    added components

---
+++ src/components/Headings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+
+function Headings() {
+  return (
+    <div>
+      <h1 className='title'>Weatherer</h1>
+      <p>Find out the temperature of anywhere!</p>
+    </div>
+  )
+}
+
+export default Headings
