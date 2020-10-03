58461265064175f912816ef823d5c2d14f4f4d47
faisalsayed10 <faisal.sayed502@gmail.com>
2020-10-03 13:12:13 +0530

    Added API key to .env file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Headings from './components/Headings';
+import Weather from './components/Weather';
 
 function App() {
+
   return (
     <div className="App">
-      
+      <Headings />
+      <Form />
+      <Weather />
     </div>
   );
 }
